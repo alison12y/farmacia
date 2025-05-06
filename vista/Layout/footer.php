743743a67bef3c21a00193cebdd3f07e1077fc19
alison12y <alisonyeseniacanavirimamani@gmail.com>
2025-05-05 21:55:35 -0400

    subiendo proyecto

--- vista/Layout/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Main Footer -->
-<footer class="main-footer">
-  <div class="float-right d-none d-sm-block">
-    <b>Versión</b> 3.0.4
-  </div>
-  <strong>Copyright &copy; 2025 
-    <a href="#">Farmacia Britmann</a>.
-  </strong> Todos los derechos reservados.
-</footer>
